49b51ae267e45c930c4ea446c75590c09c2bb922
EricklisCruz <erickliscruz@gmail.com>
2024-02-12 15:00:02 -0300

    created backend project

---
+++ backend-form-application/src/main/java/com/redway/backendformapplication/BackendFormApplication.java
@@ -0,0 +1,13 @@
+package com.redway.backendformapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFormApplication.class, args);
+	}
+
+}
---
+++ backend-form-application/src/test/java/com/redway/backendformapplication/BackendFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redway.backendformapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
