25da95c95999fe68b23cfde2986a78d64a543b1e
EricklisCruz <erickliscruz@gmail.com>
2024-02-12 16:14:08 -0300

    created method findAll users

---
+++ backend-form-application/src/main/java/com/redway/backendformapplication/service/UserService.java
@@ -0,0 +1,21 @@
+package com.redway.backendformapplication.service;
+
+import com.redway.backendformapplication.domain.user.User;
+import com.redway.backendformapplication.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
