819defc8551d541a1a361c102199ed3dc646e6eb
EricklisCruz <erickliscruz@gmail.com>
2024-02-12 15:33:15 -0300

    entity user

---
+++ backend-form-application/src/main/java/com/redway/backendformapplication/domain/user/User.java
@@ -0,0 +1,48 @@
+package com.redway.backendformapplication.domain.user;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
